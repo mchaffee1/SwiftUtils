4e6b6b8cb7e71c5314de12f75245935375f5b38c
Michael Chaffee <mchaffee@gmail.com>
2015-10-16 09:25:04 -0700

    Initial Commit

---
+++ Controls/DatePickerPop/DatePickerPopViewController.swift
@@ -0,0 +1,61 @@
+//
+//  DatePickerActionSheet.swift
+//  iDoctors
+//
+//  Created by Valerio Ferrucci on 30/09/14.
+//  Copyright (c) 2014 Tabasoft. All rights reserved.
+//
+
+import UIKit
+
+protocol DatePickerViewControllerDelegate : class {
+    
+    func datePickerVCDismissed(date : NSDate?)
+}
+
+class PopDateViewController : UIViewController {
+    
+    @IBOutlet weak var container: UIView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    weak var delegate : DatePickerViewControllerDelegate?
+
+    var currentDate : NSDate? {
+        didSet {
+            updatePickerCurrentDate()
+        }
+    }
+
+    convenience init() {
+
+        self.init(nibName: "DatePickerPopViewController", bundle: nil)
+    }
+
+    private func updatePickerCurrentDate() {
+        
+        if let _currentDate = self.currentDate {
+            if let _datePicker = self.datePicker {
+                _datePicker.date = _currentDate
+            }
+        }
+    }
+
+    @IBAction func okAction(sender: AnyObject) {
+        
+        self.dismissViewControllerAnimated(true) {
+            
+            let nsdate = self.datePicker.date
+            self.delegate?.datePickerVCDismissed(nsdate)
+            
+        }
+    }
+    
+    override func viewDidLoad() {
+        
+        updatePickerCurrentDate()
+    }
+    
+    override func viewDidDisappear(animated: Bool) {
+        
+        self.delegate?.datePickerVCDismissed(nil)
+    }
+}
---
+++ SwiftUtils/SwiftUtils.h
@@ -0,0 +1,19 @@
+//
+//  SwiftUtils.h
+//  SwiftUtils
+//
+//  Created by Michael Chaffee on 10/16/15.
+//  Copyright © 2015 Michael Chaffee. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftUtils.
+FOUNDATION_EXPORT double SwiftUtilsVersionNumber;
+
+//! Project version string for SwiftUtils.
+FOUNDATION_EXPORT const unsigned char SwiftUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftUtils/PublicHeader.h>
+
+
